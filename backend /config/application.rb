6d70ca0569b095aef0bed61dac32c0ee8a8fd124
Waseem Nafisi <waseemnafisi@gmail.com>
2018-09-21 18:28:32 -0400

    deploying on heroku added cors.rb file

--- backend /config/application.rb
+++ backend /config/application.rb
@@ -14,12 +14,12 @@ config.api_only = true
 
     config.load_defaults 5.2
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => :any
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => :any
+    #   end
+    # end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
